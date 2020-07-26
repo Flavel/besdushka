b743f80c91756775bb16ccc7074e9bcac06d839a
Flavel <earr111@mail.ru>
2020-07-26 21:35:37 +0300

    2

---
+++ Besdushka/EnterPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Besdushka
+{
+    public partial class EnterPage : ContentPage
+    {
+        public EnterPage()
+        {
+            InitializeComponent();
+            Title = "Поиск комнаты";
+        }
+    }
+}
